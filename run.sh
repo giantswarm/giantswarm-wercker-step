e61231648d1b737afc36809272e6099ea6582089
Kord Campbell <kordless@gmail.com>
2015-07-18 10:41:21 -0700

    fix chmod, bumping version

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ jq=$WERCKER_STEP_ROOT/jq
 curl=$WERCKER_STEP_ROOT/curl
 
 # get the token by calling the API
-mkdir -p $HOME/.swarm
+mkdir -p $HOME/.swarm; touch $HOME/.swarm/token
 $curl -sS \
     -H "Content-Type: application/json" \
     -X POST \
     --data '{"password":"'"$(echo -n $WERCKER_GIANTSWARM_PASS | base64)"'"}' \
     https://api.giantswarm.io/v1/user/$WERCKER_GIANTSWARM_USER/login \
-    | $jq -r '.data.Id' > $HOME/.swarm/token
+    | $jq -r '.data.Id' >> $HOME/.swarm/token
 
 # If we have an environment set, switch to it
 if [ -n "$WERCKER_GIANTSWARM_ENV" ]; then
